d7b87170f73f6df116071dda19eff6ed3173f2a8
ST3VI3-RICHI3 <st3vi3.richi3@gmail.com>
2023-02-15 05:23:14 +0000

    Add more code stuff yes

--- helloworld/src/main.rs
+++ helloworld/src/main.rs
@@ -6,11 +6,15 @@ fn main() {
     let num2 = 2;
 
     println!("Hello, world!");
-    println!("{num1} + {num2} is {}, -1 thats {} quick mafs.", num1+num2, (num1+num2)-1 );
+    println!("{num1} + {num2} is {}, -1 thats {} quick mafs.", addnums(&num1, &num2), (num1+num2)-1 );
 
     print!("\nEnter something: ");
     io::stdout().flush().unwrap();
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("error: unable to read user input");
     println!("You entered: {input}");
+}
+
+fn addnums(num1:&i32, num2:&i32) -> i32 {
+    return num1+num2;
 }
